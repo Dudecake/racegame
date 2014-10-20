2116516add3dc062ba010c23ab9c5ac5a5c3e6eb
Dudecake <coen_koomen@hotmail.com>
2014-10-20 11:24:54 +0200

    lala

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,18 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openWebsite(http://www.newgrounds.com/);
+        }
     }
+    public static void openWebsite(string URL)
+        {
+            Process p1 = new Process();
+            p1.StartInfo.FileName = "chrome.exe";
+            p1.StartInfo.Arguments = URL;
+            p1.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            p1.Start();
+        }
 }
