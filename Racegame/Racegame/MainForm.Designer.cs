788459ea9c16387c30e0dd5924b993ce3743b35b
Dudecake <coen_koomen@hotmail.com>
2014-10-22 12:32:44 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 3d243c99d86aa367de451a0fb77eae63528832aa, reversing
    changes made to 59095e1ffefd4d8b8c2730c747996364fb1629c1.

--- Racegame/Racegame/MainForm.Designer.cs
+++ Racegame/Racegame/MainForm.Designer.cs
@@ -28,22 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.screen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // screen
+            // 
+            this.screen.Location = new System.Drawing.Point(12, 12);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(1000, 744);
+            this.screen.TabIndex = 0;
+            this.screen.TabStop = false;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Controls.Add(this.screen);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Form1";
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.PictureBox screen;
 
     }
 }
