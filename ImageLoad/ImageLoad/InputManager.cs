ad3b3ee69407ba064abadb297b06daef538e802b
emprax <keizer95@live.nl>
2014-10-23 13:42:26 +0200

    Image implenter

---
+++ ImageLoad/ImageLoad/InputManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageLoad
+{
+    class InputManager
+    {
+        public Point mousePoint;
+        public Keys[] keysPressed;
+        public Keys[] keysHeld;
+        public Stopwatch gameTime;
+        public float deltaTime;
+
+        public void Update(Point mp, Keys[] kp, Keys[] kh, Stopwatch gt, float dt)
+        {
+            mousePoint = mp;
+            keysPressed = kp;
+            keysHeld = kh;
+            gameTime = gt;
+            deltaTime = dt;
+        }
+
+
+    }
+}
---
+++ ImageLoad/ImageLoad/Spritebatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageLoad
+{
+    class Spritebatch
+    {
+        private Graphics Gfx;
+        private BufferedGraphics bfgfx;
+        private BufferedGraphicsContext cntxt = BufferedGraphicsManager.Current;
+
+        public Spritebatch(Size clientsize, Graphics gfx)
+        {
+            cntxt.MaximumBuffer = new Size(clientsize.Width + 1, clientsize.Height + 1);
+            bfgfx = cntxt.Allocate(gfx, new Rectangle(Point.Empty, clientsize));
+            Gfx = gfx;
+        }
+
+        public void Begin()
+        {
+            bfgfx.Graphics.Clear(Color.Purple);
+        }
+
+        public void Draw(Sprite s)
+        {
+            bfgfx.Graphics.DrawImageUnscaled(s.Texture, s.ToRec);
+        }
+
+        public void drawImage(Bitmap b, Rectangle rec)
+        {
+            bfgfx.Graphics.DrawImageUnscaled(b, rec);
+        }
+
+        public void End()
+        {
+            bfgfx.Render(Gfx);
+        }
+    }
+}
