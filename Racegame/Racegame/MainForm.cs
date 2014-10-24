e85cd6316e6e51ef7ed94eed754ca51e80201cb6
Dudecake <coen_koomen@hotmail.com>
2014-10-24 12:29:31 +0200

    PLAYER 2 WERKERT!!!!!!111ONE111!!!!

--- Racegame/Racegame/MainForm.cs
+++ Racegame/Racegame/MainForm.cs
@@ -21,7 +21,7 @@ namespace WindowsFormsApplication1
         Collision collision = new Collision();
         //Vector vector = new Vector();
         Vehicle vehicle = new Vehicle();
-        Vehicle vehicle2 = new Vehicle();
+        Vehicle2 vehicle2 = new Vehicle2();
         //RigidBody rigidBody = new RigidBody();
         Bitmap m_map = new Bitmap(Properties.Resources.design_1, 341, 256);
 
@@ -48,6 +48,10 @@ namespace WindowsFormsApplication1
         float throttle = 0; //0 is coasting, 1 is full throttle
         float brakes = 0; //0 is no brakes, 1 is full brakes
         
+        float steering2 = 0; //-1 is full left, 0 is center, 1 is full right
+        float throttle2 = 0; //0 is coasting, 1 is full throttle
+        float brakes2 = 0; //0 is no brakes, 1 is full brakes
+
         //double time = 1; 
 
         Bitmap Backbuffer;
@@ -125,10 +129,10 @@ namespace WindowsFormsApplication1
             graphics = Graphics.FromImage(backbuffer);
             gameTime.Start();
             timer.GetETime(); //reset timer
-
-            vehicle.Setup(new Vector(7, 13) / 2.0f, 5, Color.Red);
+            Bitmap auto = new Bitmap(Properties.Resources.Z_Type_GTA2);
+            vehicle.Setup(new Vector(7, 13) / 2.0f, 5, auto);
             vehicle.SetLocation(new Vector(210, -7), 0);
-            vehicle2.Setup(new Vector(7, 13) / 2.0f, 5, Color.Red);
+            vehicle2.Setup(new Vector(7, 13) / 2.0f, 5, auto);
             vehicle2.SetLocation(new Vector(190, -7), 0);
         }
 
@@ -155,40 +159,6 @@ namespace WindowsFormsApplication1
             vehicle.Draw(graphics, buffersize);
             vehicle2.Draw(graphics, buffersize);
         }
-        private void DoFrame2()
-        {
-            //get elapsed time since last frame
-            float etime = timer.GetETime();
-
-            //process input
-            ProcessInput2();
-
-            //apply vehicle controls
-            vehicle2.SetSteering(steering);
-            vehicle2.SetThrottle(throttle, true); //menu.Checked
-            vehicle2.SetBrakes(brakes);
-
-            //integrate vehicle physics
-            vehicle2.Update(etime);
-
-            //keep the vehicle on the screen
-            ConstrainVehicle2();
-
-            //CheckFps();
-
-            //redraw our screen
-            screen.Invalidate();
-
-            try
-            {
-                this.Text = String.Format("{0}FPS", fps);
-            }
-            catch
-            {
-                Console.Write("i");
-            }
-
-        }
         
         //process game logic
         private void DoFrame()
@@ -198,19 +168,25 @@ namespace WindowsFormsApplication1
 
             //process input
             ProcessInput();
+            ProcessInput2();
 
             //apply vehicle controls
             vehicle.SetSteering(steering);
             vehicle.SetThrottle(throttle, true); //menu.Checked
             vehicle.SetBrakes(brakes);
+            vehicle2.SetSteering(steering2);
+            vehicle2.SetThrottle(throttle2, true); //menu.Checked
+            vehicle2.SetBrakes(brakes2);
 
             //integrate vehicle physics
             vehicle.Update(etime);
+            vehicle2.Update(etime);
 
             //keep the vehicle on the screen
             ConstrainVehicle();
+            ConstrainVehicle2();
 
-            //CheckFps();
+            CheckFps();
 
             //redraw our screenconstra
             screen.Invalidate();
@@ -318,21 +294,21 @@ namespace WindowsFormsApplication1
         private void ProcessInput()
         {
             if (AHeld)
-                steering = -1;
+                steering2 = -1;
             else if (DHeld)
-                steering = 1;
+                steering2 = 1;
             else
-                steering = 0;
+                steering2 = 0;
 
             if (WHeld)
-                throttle = 1;
+                throttle2 = 1;
             else
-                throttle = 0;
+                throttle2 = 0;
 
             if (SHeld)
-                brakes = 12;
+                brakes2 = 12;
             else
-                brakes = 0.4f;
+                brakes2 = 0.4f;
         }
 
         private void onKeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
@@ -433,8 +409,7 @@ namespace WindowsFormsApplication1
         {
             // While the application is still idle, run frame routine.
             DoFrame();
-            DoFrame2();
-            CheckFps();
+            //CheckFps();
         }
         
         /*
@@ -540,7 +515,7 @@ namespace WindowsFormsApplication1
             {
                 fpsTime = gameTime.ElapsedMilliseconds;
                 fps = fpsCounter;
-                fpsCounter = 0; 
+                fpsCounter = 0;
             }
             else
             {
@@ -638,7 +613,7 @@ namespace WindowsFormsApplication1
         }
         private Wheel [] wheels = new Wheel[4];
 
-        new public void Setup(Vector halfSize, float mass, Color color)
+        new public void Setup(Vector halfSize, float mass, Bitmap color)
         {
             //front wheels
             wheels[0] = new Wheel(new Vector(halfSize.X, halfSize.Y), 0.5f);
@@ -724,7 +699,7 @@ namespace WindowsFormsApplication1
         //graphical properties
         private Vector m_halfSize = new Vector();
         Rectangle rect = new Rectangle();
-        private Color m_color;
+        //private Color m_color;
         private Bitmap m_bitmap = new Bitmap(Properties.Resources.Z_Type_GTA2);
  
         public RigidBody()
@@ -735,12 +710,12 @@ namespace WindowsFormsApplication1
         }
 
         //intialize out parameters
-        public void Setup(Vector halfSize, float mass, Color color)
+        public void Setup(Vector halfSize, float mass, Bitmap color)
         {
             //store physical parameters
             m_halfSize = halfSize;
             m_mass = mass;
-            m_color = color;
+            m_bitmap = color;
             m_inertia = (1.0f / 12.0f) * (halfSize.X * halfSize.X) * (halfSize.Y * halfSize.Y) * mass;
 
             //generate our viewable rectangle
@@ -933,6 +908,301 @@ namespace WindowsFormsApplication1
             float thisDotV = this * v;
             mag = thisDotV;
             return v * thisDotV;
+        }
+    }
+
+    class Vehicle2 : RigidBody2
+    {
+        private class Wheel
+        {
+            private Vector m_forwardAxis, m_sideAxis;
+            private float m_wheelTorque, m_wheelSpeed, m_wheelInertia, m_wheelRadius;
+            private Vector m_Position = new Vector();
+
+            public Wheel(Vector position, float radius)
+            {
+                m_Position = position;
+                SetSteeringAngle(0);
+                m_wheelSpeed = 0;
+                m_wheelRadius = radius;
+                m_wheelInertia = radius * radius; //fake value
+            }
+
+            public void SetSteeringAngle(float newAngle)
+            {
+                Matrix mat = new Matrix();
+                PointF[] vectors = new PointF[2];
+
+                //foward vector
+                vectors[0].X = 0;
+                vectors[0].Y = 1;
+                //side vector
+                vectors[1].X = -1;
+                vectors[1].Y = 0;
+
+                mat.Rotate(newAngle / (float)Math.PI * 180.0f);
+                mat.TransformVectors(vectors);
+
+                m_forwardAxis = new Vector(vectors[0].X, vectors[0].Y);
+                m_sideAxis = new Vector(vectors[1].X, vectors[1].Y);
+            }
+
+            public void AddTransmissionTorque(float newValue)
+            {
+                m_wheelTorque += newValue;
+            }
+
+            public float GetWheelSpeed()
+            {
+                return m_wheelSpeed;
+            }
+
+            public Vector GetAttachPoint()
+            {
+                return m_Position;
+            }
+
+            public Vector CalculateForce(Vector relativeGroundSpeed, float timeStep)
+            {
+                //calculate speed of tire patch at ground
+                Vector patchSpeed = -m_forwardAxis * m_wheelSpeed * m_wheelRadius;
+
+                //get velocity difference between ground and patch
+                Vector velDifference = relativeGroundSpeed + patchSpeed;
+
+                //project ground speed onto side axis
+                float forwardMag = 0;
+                Vector sideVel = velDifference.Project(m_sideAxis);
+                Vector forwardVel = velDifference.Project(m_forwardAxis, out forwardMag);
+
+                //calculate super fake friction forces
+                //calculate response force
+                Vector responseForce = -sideVel * 2.0f;
+                responseForce -= forwardVel;
+
+                //calculate torque on wheel
+                m_wheelTorque += forwardMag * m_wheelRadius;
+
+                //integrate total torque into wheel
+                m_wheelSpeed += m_wheelTorque / m_wheelInertia * timeStep;
+
+                //clear our transmission torque accumulator
+                m_wheelTorque = 0;
+
+                //return force acting on body
+                return responseForce;
+            }
+        }
+        private Wheel[] wheels = new Wheel[4];
+
+        new public void Setup(Vector halfSize, float mass, Bitmap color)
+        {
+            //front wheels
+            wheels[0] = new Wheel(new Vector(halfSize.X, halfSize.Y), 0.5f);
+            wheels[1] = new Wheel(new Vector(-halfSize.X, halfSize.Y), 0.5f);
+
+            //rear wheels
+            wheels[2] = new Wheel(new Vector(halfSize.X, -halfSize.Y), 0.5f);
+            wheels[3] = new Wheel(new Vector(-halfSize.X, -halfSize.Y), 0.5f);
+
+            base.Setup(halfSize, mass, color);
+        }
+
+        public void SetSteering(float steering)
+        {
+            const float steeringLock = 0.4f;
+
+            //apply steering angle to front wheels
+            wheels[0].SetSteeringAngle(-steering * steeringLock);
+            wheels[1].SetSteeringAngle(-steering * steeringLock);
+        }
+
+        public void SetThrottle(float throttle, bool allWheel)
+        {
+            const float torque = 60.0f;
+
+            //apply transmission torque to back wheels
+            if (allWheel)
+            {
+                wheels[0].AddTransmissionTorque(throttle * torque / 2);
+                wheels[1].AddTransmissionTorque(throttle * torque / 2);
+            }
+
+            wheels[2].AddTransmissionTorque(throttle * torque);
+            wheels[3].AddTransmissionTorque(throttle * torque);
+        }
+
+        public void SetBrakes(float brakes)
+        {
+            const float brakeTorque = 0.7f;
+
+
+            //apply brake torque apposing wheel vel
+            foreach (Wheel wheel in wheels)
+            {
+                float wheelVel = wheel.GetWheelSpeed();
+                wheel.AddTransmissionTorque(-wheelVel * brakeTorque * brakes);
+            }
+        }
+
+        new public void Update(float timeStep)
+        {
+            foreach (Wheel wheel in wheels)
+            {
+                //wheel.m_wheelSpeed = 30.0f;
+                Vector worldWheelOffset = base.RelativeToWorld(wheel.GetAttachPoint());
+                Vector worldGroundVel = base.PointVel(worldWheelOffset);
+                Vector relativeGroundSpeed = base.WorldToRelative(worldGroundVel);
+                Vector relativeResponseForce = wheel.CalculateForce(relativeGroundSpeed, timeStep);
+                Vector worldResponseForce = base.RelativeToWorld(relativeResponseForce);
+
+                base.AddForce(worldResponseForce, worldWheelOffset);
+            }
+
+            base.Update(timeStep);
+        }
+    }
+
+    //our simulation object
+    class RigidBody2
+    {
+        //linear properties
+        private Vector m_position = new Vector();
+        private Vector m_velocity = new Vector();
+        private Vector m_forces = new Vector();
+        private float m_mass;
+
+        //angular properties
+        private float m_angle;
+        private float m_angularVelocity;
+        private float m_torque;
+        private float m_inertia;
+
+        //graphical properties
+        private Vector m_halfSize = new Vector();
+        Rectangle rect = new Rectangle();
+        //private Color m_color;
+        private Bitmap m_bitmap; // = new Bitmap(Properties.Resources.Z_Type_GTA2);
+
+        public RigidBody2()
+        {
+            //set these defaults so we dont get divide by zeros
+            m_mass = 1.0f;
+            m_inertia = 1.0f;
+        }
+
+        //intialize out parameters
+        public void Setup(Vector halfSize, float mass, Bitmap color)
+        {
+            //store physical parameters
+            m_halfSize = halfSize;
+            m_mass = mass;
+            m_bitmap = color;
+            m_inertia = (1.0f / 12.0f) * (halfSize.X * halfSize.X) * (halfSize.Y * halfSize.Y) * mass;
+
+            //generate our viewable rectangle
+            rect.X = (int)-m_halfSize.X;
+            rect.Y = (int)-m_halfSize.Y;
+            rect.Width = (int)(m_halfSize.X * 2.0f);
+            rect.Height = (int)(m_halfSize.Y * 2.0f);
+        }
+
+        public void SetLocation(Vector position, float angle)
+        {
+            m_position = position;
+            m_angle = angle;
+        }
+
+        public Vector GetPosition()
+        {
+            return m_position;
+        }
+
+        public void Update(float timeStep)
+        {
+            //integrate physics
+            //linear
+            Vector acceleration = m_forces / m_mass;
+            m_velocity += acceleration * timeStep;
+            m_position += m_velocity * timeStep;
+            m_forces = new Vector(0, 0); //clear forces
+
+            //angular
+            float angAcc = m_torque / m_inertia;
+            m_angularVelocity += angAcc * timeStep;
+            m_angle += m_angularVelocity * timeStep;
+            m_torque = 0; //clear torque
+        }
+
+        public void Draw(Graphics graphics, Size buffersize)
+        {
+            //store transform, (like opengl's glPushMatrix())
+            Matrix mat1 = graphics.Transform;
+
+            //transform into position
+            graphics.TranslateTransform(m_position.X, m_position.Y);
+            graphics.RotateTransform(m_angle / (float)Math.PI * 180.0f);
+
+            try
+            {
+                //draw body
+                //graphics.DrawRectangle(new Pen(m_color), rect);
+                graphics.DrawImage(m_bitmap, rect);
+                //draw line in the "forward direction"
+                //graphics.DrawLine(new Pen(Color.Yellow), 1, 0, 1, 5);
+            }
+            catch (OverflowException exc)
+            {
+                //physics overflow :(
+            }
+
+            //restore transform
+            graphics.Transform = mat1;
+        }
+
+        //take a relative vector and make it a world vector
+        public Vector RelativeToWorld(Vector relative)
+        {
+            Matrix mat = new Matrix();
+            PointF[] vectors = new PointF[1];
+
+            vectors[0].X = relative.X;
+            vectors[0].Y = relative.Y;
+
+            mat.Rotate(m_angle / (float)Math.PI * 180.0f);
+            mat.TransformVectors(vectors);
+
+            return new Vector(vectors[0].X, vectors[0].Y);
+        }
+
+        //take a world vector and make it a relative vector
+        public Vector WorldToRelative(Vector world)
+        {
+            Matrix mat = new Matrix();
+            PointF[] vectors = new PointF[1];
+
+            vectors[0].X = world.X;
+            vectors[0].Y = world.Y;
+
+            mat.Rotate(-m_angle / (float)Math.PI * 180.0f);
+            mat.TransformVectors(vectors);
+
+            return new Vector(vectors[0].X, vectors[0].Y);
+        }
+
+        //velocity of a point on body
+        public Vector PointVel(Vector worldOffset)
+        {
+            Vector tangent = new Vector(-worldOffset.Y, worldOffset.X);
+            return tangent * m_angularVelocity + m_velocity;
+        }
+
+        public void AddForce(Vector worldForce, Vector worldOffset)
+        {
+            //add linar force
+            m_forces += worldForce;
+            //and it's associated torque
+            m_torque += worldOffset % worldForce;
         }
     }
 
