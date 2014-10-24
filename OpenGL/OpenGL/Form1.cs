06a565c5ead7f69197b2b2a9dcef4713dd5700e5
Dudecake <coen_koomen@hotmail.com>
2014-10-24 18:54:52 +0200

    Come to OpenGL, we have Vector2

---
+++ OpenGL/OpenGL/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace OpenGL
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch sw = new Stopwatch();
+        bool loaded = false;
+        int x = 0;
+        float rotation = 0;
+        double accumulator = 0;
+        int idleCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            loaded = true;
+            GL.ClearColor(Color.HotPink);
+            SetupViewport();
+            Application.Idle += Application_Idle;
+            sw.Start();
+        }
+
+        private void SetupViewport()
+        {
+            int w = glControl1.Width;
+            int h = glControl1.Height;
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, w, 0, h, -1, 1);
+            GL.Viewport(0, 0, w, h);
+        }
+        private void Render()
+        {
+            if (!loaded) return;
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.ColorBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Translate(x, 0, 0);
+            if (glControl1.Focused) GL.Color3(Color.Yellow);
+            else GL.Color3(Color.Blue);
+            GL.Rotate(rotation, Vector3.UnitZ);
+            GL.Begin(BeginMode.Triangles);
+            GL.Vertex2(10, 20);
+            GL.Vertex2(100, 20);
+            GL.Vertex2(100, 50);
+            GL.End();
+
+            glControl1.SwapBuffers();
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        private void glControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                x += 10;
+                glControl1.Invalidate();
+            }
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            SetupViewport();
+            glControl1.Invalidate();
+        }
+        void Application_Idle(object sender, EventArgs e)
+        {
+            double milliseconds = ComputeTimeSlice();
+            Accumulate(milliseconds);
+            Animate(milliseconds);
+        }
+        private double ComputeTimeSlice()
+        {
+            sw.Stop();
+            double timeslice = sw.Elapsed.TotalMilliseconds;
+            sw.Reset();
+            sw.Start();
+            return timeslice;
+        }
+        private void Animate(double milliseconds)
+        {
+            float deltaRotation = (float)milliseconds / 20.0f;
+            rotation += deltaRotation;
+            glControl1.Invalidate();
+        }
+        private void Accumulate(double milliseconds)
+        {
+            idleCounter++;
+            accumulator += milliseconds;
+            if (accumulator > 1000)
+            {
+                label1.Text = idleCounter.ToString();
+                accumulator = 0;
+                idleCounter = 0;
+            }
+        }
+    }
+}
