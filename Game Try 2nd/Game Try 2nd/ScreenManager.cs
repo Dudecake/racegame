ff2963101ae44e6a0601731b13f3efa0075fa79d
emprax <keizer95@live.nl>
2014-10-24 20:21:38 +0200

    collision map so far
    heeft nog een paar problemen: krijg het niet op een picturebox, past om
    de een of andere reden niet in de form.

---
+++ Game Try 2nd/Game Try 2nd/ScreenManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Try_2nd
+{
+    class ScreenManager
+    {
+        public enum GameState { Splash, Menu, Game };
+
+        public ScreenManager()
+        {
+
+        }
+
+        public void LoadContent(InputManager iManager)
+        {
+
+        }
+
+        public void Update(InputManager iManager)
+        {
+
+        }
+        
+        public void Draw(Spritebatch sb)
+        {
+
+        }
+    }
+}
