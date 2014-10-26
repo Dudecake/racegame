b0d79465d1de0a851b5eba53f7483a1c02e026e3
emprax <keizer95@live.nl>
2014-10-26 18:38:33 +0100

    Latest version of the Stopwatch system
    nog niet volledig compleet maar er is wel genoeg om wat mee te kunnen.

--- player logic/player logic/Form1.Designer.cs
+++ player logic/player logic/Form1.Designer.cs
@@ -39,6 +39,20 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -47,33 +61,33 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Start";
+            this.button1.Text = "player1starts";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(270, 15);
+            this.button2.Location = new System.Drawing.Point(13, 94);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Stop";
+            this.button2.Text = "player1finished";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(270, 42);
+            this.button3.Location = new System.Drawing.Point(13, 238);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Stop";
+            this.button3.Text = "player2finished";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(13, 142);
+            this.button4.Location = new System.Drawing.Point(23, 394);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 3;
@@ -82,51 +96,54 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(12, 45);
+            this.button5.Location = new System.Drawing.Point(12, 151);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 4;
-            this.button5.Text = "Start";
+            this.button5.Text = "player2starts";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(13, 202);
+            this.button6.Location = new System.Drawing.Point(13, 38);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
+            this.button6.Text = "1stRoundOver";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(13, 266);
+            this.button7.Location = new System.Drawing.Point(13, 65);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 23);
             this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
+            this.button7.Text = "2ndRoundOver";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(13, 296);
+            this.button8.Location = new System.Drawing.Point(12, 180);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(75, 23);
             this.button8.TabIndex = 7;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(94, 15);
+            this.textBox1.Location = new System.Drawing.Point(93, 94);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(170, 20);
             this.textBox1.TabIndex = 8;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(94, 45);
+            this.textBox2.Location = new System.Drawing.Point(93, 241);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(170, 20);
             this.textBox2.TabIndex = 9;
@@ -134,17 +151,162 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(94, 147);
+            this.label1.Location = new System.Drawing.Point(93, 185);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 10;
-            this.label1.Text = "label1";
+            this.label1.Text = "1stRound";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "2ndRound";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "1stRound";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 209);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(296, 33);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "best time";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(93, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "2ndRound";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(163, 185);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(163, 214);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(390, 38);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "label9";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(296, 175);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 21;
+            this.button11.Text = "best time";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(390, 180);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "label10";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(12, 295);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 23;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(93, 300);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "label11";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(411, 360);
+            this.ClientSize = new System.Drawing.Size(649, 481);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -176,6 +338,20 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label11;
     }
 }
 
--- player logic/player logic/Form1.cs
+++ player logic/player logic/Form1.cs
@@ -16,6 +16,14 @@ namespace player_logic
     {
         Stopwatch stopWatch;
         Stopwatch stopHorloge;
+        public System.TimeSpan[] Timesp1;
+        public System.TimeSpan[] Timesp2;
+        public bool Alpha;
+        public bool Beta;
+        public bool Gamma;
+        public bool Delta;
+        public bool Phi;
+        public bool Omega;
 
         public Form1()
         {
@@ -23,6 +31,16 @@ namespace player_logic
 
             stopWatch = new Stopwatch();
             stopHorloge = new Stopwatch();
+            Timesp1 = new System.TimeSpan[3];
+            Timesp2 = new System.TimeSpan[3];
+            Alpha = false;
+            Beta = false;
+            Gamma = false;
+            Delta = false;
+            Phi = false;
+            Omega = false;
+            label11.Visible = false;
+            label11.Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -33,7 +51,15 @@ namespace player_logic
         private void button2_Click(object sender, EventArgs e)
         {
             stopWatch.Stop();
-            textBox1.Text = string.Format("{0}", stopWatch.Elapsed);
+            bool a = true;
+            if(a)
+            {
+                //int a3 = Convert.ToInt32(stopWatch.Elapsed);
+                Timesp1[2] = stopWatch.Elapsed;
+                textBox1.Text = string.Format("{0}", stopWatch.Elapsed);
+                //Timesp1[2] = Convert.ToInt32(stopWatch.Elapsed);
+                Alpha = true;
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -44,7 +70,105 @@ namespace player_logic
         private void button3_Click(object sender, EventArgs e)
         {
             stopHorloge.Stop();
-            textBox2.Text = string.Format("{0}", stopHorloge.Elapsed);
+            bool b = true;
+            if(b)
+            {
+                //int b3 = Convert.ToInt32(stopHorloge.Elapsed);
+                Timesp2[2] = stopHorloge.Elapsed;
+                textBox2.Text = string.Format("{0}", stopHorloge.Elapsed);
+                //Timesp2[2] = Convert.ToInt32(stopHorloge.Elapsed);
+                Beta = true;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            bool a = true;
+            //int a1 = Convert.ToInt32(stopWatch.Elapsed);
+            if (a == true)
+            {
+                label2.Text = string.Format("{0}", stopWatch.Elapsed);
+                Timesp1[0] = stopWatch.Elapsed;
+                //Timesp1[0] = Convert.ToInt32(stopWatch.Elapsed);
+                stopWatch.Restart();
+                Gamma = true;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            bool b = true;
+            //int a2 = Convert.ToInt32(stopWatch.Elapsed);
+            if(b == true)
+            {
+                label3.Text = string.Format("{0}", stopWatch.Elapsed);
+                Timesp1[1] = stopWatch.Elapsed;
+                //Timesp1[1] = Convert.ToInt32(stopWatch.Elapsed);
+                Delta = true;
+                stopWatch.Restart();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            bool c = true;
+            //int b1 = Convert.ToInt32(stopHorloge.Elapsed);
+            if(c == true)
+            {
+                label7.Text = string.Format("{0}", stopHorloge.Elapsed);
+                Timesp2[0] = stopHorloge.Elapsed;
+                //Timesp2[0] = Convert.ToInt32(stopHorloge.Elapsed);
+                Phi = true;
+                stopWatch.Restart();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            bool d = true;
+            //int b2 = Convert.ToInt32(stopHorloge.Elapsed);
+            if(d == true)
+            {
+                label8.Text = string.Format("{0}", stopHorloge.Elapsed);
+                Timesp2[1] = stopHorloge.Elapsed;
+                //Timesp2[1] = Convert.ToInt32(stopHorloge.Elapsed);
+                Omega = true;
+                stopWatch.Restart();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Timesp1.Min();
+            label9.Text = String.Format("{0}", Timesp1.Min());
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Timesp2.Min();
+            label10.Text = String.Format("{0}", Timesp2.Min());
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if(Alpha && Beta && Gamma && Delta && Phi && Omega)
+            {
+                if(Timesp1.Min() < Timesp2.Min())
+                {
+                    label11.Visible = true;
+                    label11.Text = "Player 1 has got the best time";
+                }
+                else if(Timesp2.Min() < Timesp1.Min())
+                {
+                    label11.Visible = true;
+                    label11.Text = "Player 2 has got the best time";
+                }
+            }
         }
     }
 }
