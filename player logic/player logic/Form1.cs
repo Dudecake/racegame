ccce65b7161c46f50cb8c57e70074030a3b15f75
emprax <keizer95@live.nl>
2014-11-16 20:08:14 +0100

    changes

--- player logic/player logic/Form1.cs
+++ player logic/player logic/Form1.cs
@@ -15,7 +15,6 @@ namespace player_logic
     public partial class Form1 : Form
     {
         Stopwatch stopWatch;
-        Stopwatch stopHorloge;
         public System.TimeSpan[] Timesp1;
         public System.TimeSpan[] Timesp2;
         public bool Alpha;
@@ -30,17 +29,14 @@ namespace player_logic
             InitializeComponent();
 
             stopWatch = new Stopwatch();
-            stopHorloge = new Stopwatch();
-            Timesp1 = new System.TimeSpan[3];
             Timesp2 = new System.TimeSpan[3];
+            Timesp1 = new System.TimeSpan[3];
             Alpha = false;
             Beta = false;
             Gamma = false;
             Delta = false;
             Phi = false;
             Omega = false;
-            label11.Visible = false;
-            label11.Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -50,30 +46,23 @@ namespace player_logic
 
         private void button2_Click(object sender, EventArgs e)
         {
-            stopWatch.Stop();
             bool a = true;
-            if(a)
+            if (a)
             {
                 Timesp1[2] = stopWatch.Elapsed;
-                textBox1.Text = string.Format("{0}", stopWatch.Elapsed);
+                textBox1.Text = string.Format("{0}", Timesp1[2]);
                 Alpha = true;
             }
             int i = Convert.ToInt32(stopHorloge.Elapsed);
         }
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            stopHorloge.Start();
-        }
-
         private void button3_Click(object sender, EventArgs e)
         {
-            stopHorloge.Stop();
             bool b = true;
-            if(b)
+            if (b)
             {
-                Timesp2[2] = stopHorloge.Elapsed;
-                textBox2.Text = string.Format("{0}", stopHorloge.Elapsed);
+                Timesp2[2] = stopWatch.Elapsed;
+                textBox2.Text = string.Format("{0}", Timesp2[2]);
                 Beta = true;
             }
         }
@@ -88,9 +77,8 @@ namespace player_logic
             bool a = true;
             if (a == true)
             {
-                label2.Text = string.Format("{0}", stopWatch.Elapsed);
                 Timesp1[0] = stopWatch.Elapsed;
-                stopWatch.Restart();
+                label2.Text = string.Format("{0}", Timesp1[0]);
                 Gamma = true;
             }
         }
@@ -98,81 +86,34 @@ namespace player_logic
         private void button7_Click(object sender, EventArgs e)
         {
             bool b = true;
-            if(b == true)
+            if (b == true)
             {
-                label3.Text = string.Format("{0}", stopWatch.Elapsed);
                 Timesp1[1] = stopWatch.Elapsed;
+                label3.Text = string.Format("{0}", Timesp1[1]);
                 Delta = true;
-                stopWatch.Restart();
             }
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             bool c = true;
-            if(c == true)
+            if (c == true)
             {
-                label7.Text = string.Format("{0}", stopHorloge.Elapsed);
-                Timesp2[0] = stopHorloge.Elapsed;
+                Timesp2[0] = stopWatch.Elapsed;
+                label7.Text = string.Format("{0}", Timesp2[0]);
                 Phi = true;
-                stopHorloge.Restart();
             }
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
             bool d = true;
-            if(d == true)
+            if (d == true)
             {
-                label8.Text = string.Format("{0}", stopHorloge.Elapsed);
-                Timesp2[1] = stopHorloge.Elapsed;
+                Timesp2[1] = stopWatch.Elapsed;
+                label8.Text = string.Format("{0}", Timesp2[1]);
                 Omega = true;
-                stopHorloge.Restart();
             }
         }
-
-        private void button10_Click(object sender, EventArgs e)
-        {
-            Timesp1.Min();
-            label9.Text = String.Format("{0}", Timesp1.Min());
-        }
-
-        private void button11_Click(object sender, EventArgs e)
-        {
-            Timesp2.Min();
-            label10.Text = String.Format("{0}", Timesp2.Min());
-        }
-
-        private void button12_Click(object sender, EventArgs e)
-        {
-            if(Alpha && Beta && Gamma && Delta && Phi && Omega)
-            {
-                if(Timesp1.Min() < Timesp2.Min())
-                {
-                    label11.Visible = true;
-                    label11.Text = "Player 1 has got the best time";
-                }
-                else if(Timesp2.Min() < Timesp1.Min())
-                {
-                    label11.Visible = true;
-                    label11.Text = "Player 2 has got the best time";
-                }
-            }
-        }
-
-        private void button13_Click(object sender, EventArgs e)
-        {
-            stopWatch.Reset();
-            stopHorloge.Reset();
-            textBox1.Clear();
-            textBox2.Clear();
-            label2.ResetText();
-            label3.ResetText();
-            label7.ResetText();
-            label8.ResetText();
-            label9.ResetText();
-            label10.ResetText();
-            label11.ResetText();
-        }
-    }
+    } 
 }
